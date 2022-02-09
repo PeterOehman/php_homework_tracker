9d24b9921de71795d22e15e0bf95a887860ac02c
Peter Oehman <tucker2923@gmail.com>
2022-02-09 16:22:34 -0500

    created actions in index file

---
+++ view/assignment_list.php
@@ -0,0 +1,11 @@
+<?php include('view/header.php'); ?>
+
+<section id="list" class="list">
+  <header class="list__row list__header">
+    <h1>Assignments</h1>
+    <form action="." method="get" id="list__header_select" class="list__header_select">
+  </header>
+</section>
+
+<?php include('view/footer.php'); ?>
+
